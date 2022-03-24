a80798a1046bb45e5cf1f2452234c8ea7713b5cb
Hossein <hsa150@sfu.ca>
2022-03-23 20:02:06 -0700

    add ceiling world

---
+++ dingo_simulator/dingo_gazebo/launch/dingo_ceiling.launch.py
@@ -0,0 +1,100 @@
+#! /usr/bin/env python3
+
+# Author: Hossein S.D <hsa150@sfu.ca>
+
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    # Get the launch directory
+    launch_file_dir = os.path.join(get_package_share_directory('dingo_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    world = LaunchConfiguration('world')
+    x_pose = LaunchConfiguration('x_pose')
+    y_pose = LaunchConfiguration('y_pose')
+
+
+    # Declare the launch arguments
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_x_pose_cmd = DeclareLaunchArgument(
+        'x_pose',
+        default_value='-3.0',
+        description='x position of the robot at the begining')
+
+    declare_y_pose_cmd = DeclareLaunchArgument(
+        'y_pose',
+        default_value='3.0',
+        description='y position of the robot at the begining')
+
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(
+        get_package_share_directory('dingo_gazebo'),
+        'worlds',
+        'dingo_ceiling.world'
+        ),
+        description='Full path to world model file to load')
+
+    
+
+    # Specify the actions for setting up Gazebo
+    start_gazebo_server_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    start_gazebo_client_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    robot_spawn_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_dingo.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    # Create a launch descriptor
+    ld = LaunchDescription()
+
+    # Declare launch file options
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_x_pose_cmd)
+    ld.add_action(declare_y_pose_cmd)
+    ld.add_action(declare_world_cmd)
+
+    # Add the commands to the launch description
+    ld.add_action(start_gazebo_server_cmd)
+    ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(robot_spawn_cmd)
+
+    return ld
